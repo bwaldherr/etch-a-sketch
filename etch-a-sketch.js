e817214df50dee0955ae5457af4149590266143d
Bryce Waldherr <bwall34@gmail.com>
2024-09-28 16:26:49 -0700

    added grid resize

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,26 +1,55 @@
+  /* Grid on Start */
+sqr = 16;
 
-for(let x=0; x < 16; x++){
-    for(let j=0; j < 16; j++){
+
+
+function createGrid(sqr){
+  for(let x=0; x < sqr; x++){
+    for(let j=0; j < sqr; j++){
 var div = document.createElement('div');
 div.id = 'square';
+div.style.height = "calc(100%/ "+ sqr + ")";
+div.style.width = "calc(100%/ "+ sqr +")" ;
 var container = document.querySelector('#container');
-container.appendChild(div);
+container.appendChild(div)
 }
 }
-
-var squares = document.querySelectorAll('#square');
+    /* Color MouseOver */
+    var squares = document.querySelectorAll('#square');
     squares.forEach(squares  => {
 squares.addEventListener('mouseover', () =>{
     squares.style.backgroundColor = 'blue';
 });
     });
-
-var btn = document.querySelector('button');
-
+       /* Reset Grid */ 
+var btn = document.getElementById('reset');
 btn.addEventListener('click',() => {
     squares.forEach(squares  => {
             squares.style.backgroundColor = 'white';
         });
             });
-        
+}
+createGrid(sqr);
+
+
+function clearGrid(){
+    container.textContent = "";
+}
+
+
+
+
     
+
+        
+
+  /* Resize Grid */
+var btnResize = document.getElementById('resize');
+
+btnResize.addEventListener('click', () => {
+    let x = prompt("What size would you like your grid?");
+    clearGrid();
+    createGrid(x);
+});
+
+
